323eb4549879692d734a8d2ab3fb3dcb25b6f2ad
vvnf <vivian3050@gmail.com>
2025-05-10 21:00:27 +0200

    fix: enhance content in aboutView component for better clarity and information

--- src/views/aboutView.vue
+++ src/views/aboutView.vue
@@ -2,6 +2,14 @@
   <div>
     <h1>About Page ---</h1>
     <p>This is the about page of your app .....</p>
+    <p>Here you can add information about your app, its features, and more.</p>
+    <p>Feel free to customize this page as per your requirements.</p>
+    <p>For more information, visit the official documentation.</p>
+    <p>
+      <a href="https://vuejs.org/guide/introduction.html" target="_blank"
+        >Vue.js Documentation</a
+      >
+    </p>
   </div>
 </template>
 
